2b18b7553fbdc1e47d2c28cee7ef05e78be4e2e5
Shenn2948 <shenn2948@gmail.com>
2019-09-07 17:29:33 +0300

    upd

--- Revit_Utilities/Command.cs
+++ Revit_Utilities/Command.cs
@@ -28,39 +28,19 @@ namespace Revit_Utilities
         {
             var welds = GetWeld(doc).ToList();
 
-            var welds2 = new FilteredElementCollector(doc).WhereElementIsNotElementType()
-                .WhereElementIsViewIndependent()
-                .OfCategory(BuiltInCategory.OST_PipeFitting)
-                .OfClass(typeof(FamilyInstance))
-                .Cast<FamilyInstance>()
-                .Where(i => i.Name.Equals("ГОСТ 10704-91 Трубы стальные электросварные прямошовные") && i.Symbol.FamilyName.Equals("801_СварнойШов_ОБЩИЙ"))
-                .GroupBy(
-                    i => i.MEPModel.ConnectorManager.Connectors.Cast<Connector>().SelectMany(e => e.AllRefs.Cast<Connector>()).FirstOrDefault().Owner.Name,
-                    i => i)
-                .Where(
-                    e => e.Key.Contains("Азот") || e.Key.Contains("Вода") || e.Key.Contains("Газ") || e.Key.Contains("Дренаж") || e.Key.Contains("Канализация")
-                         || e.Key.Contains("Нефтепродукты") || e.Key.Contains("Пенообразователь") || e.Key.Contains("ХимическиеРеагенты"))
-                .ToDictionary(e => e.Key, e => e.ToList());
-
             using (Transaction tran = new Transaction(doc))
             {
                 tran.Start("Change");
 
-                // Change(doc, welds, "0_153_255", "Азот");
-                // Change(doc, welds, "0_96_0", "Вода");
-                // Change(doc, welds, "255_220_112", "Газ");
-                // Change(doc, welds, "192_192_192", "Дренаж");
-                // Change(doc, welds, "192_192_192", "Канализация");
-                // Change(doc, welds, "160_80_0", "Нефтепродукты");
-                // Change(doc, welds, "224_0_0", "Пенообразователь");
-                // Change(doc, welds, "128_96_0", "ХимическиеРеагенты");
-                foreach (var item in welds2)
-                {
-                    if (item.Key.Contains("Дренаж"))
-                    {
-                        Change(doc, item.Value, "192_192_192", "Дренаж");
-                    }
-                }
+                Change(doc, welds, "0_153_255", "Азот");
+                Change(doc, welds, "0_96_0", "Вода");
+                Change(doc, welds, "255_220_112", "Газ");
+                Change(doc, welds, "192_192_192", "Дренаж");
+
+                Change(doc, welds, "192_192_192", "Канализация");
+                Change(doc, welds, "160_80_0", "Нефтепродукты");
+                Change(doc, welds, "224_0_0", "Пенообразователь");
+                Change(doc, welds, "128_96_0", "ХимическиеРеагенты");
 
                 tran.Commit();
             }
@@ -76,27 +56,81 @@ namespace Revit_Utilities
                 .Where(i => i.Name.Equals("ГОСТ 10704-91 Трубы стальные электросварные прямошовные") && i.Symbol.FamilyName.Equals("801_СварнойШов_ОБЩИЙ"));
         }
 
+        private static IEnumerable<FamilyInstance> GetPipeType(IEnumerable<FamilyInstance> welds, string pipeType)
+        {
+            return from e in welds
+                   from Connector connector in e.MEPModel.ConnectorManager.Connectors
+                   from Connector reference in connector.AllRefs
+                   where reference.Owner.Name.Contains(pipeType)
+                   select e;
+        }
+
+        private static IEnumerable<Element> GetElementsToRecolor(IEnumerable<FamilyInstance> pipes)
+        {
+            return from e in pipes
+                   from Connector connector in e.MEPModel.ConnectorManager.Connectors
+                   from Connector reference in connector.AllRefs
+                   where (reference.Owner.Category.Id.IntegerValue == (int)BuiltInCategory.OST_PipeFitting)
+                         && !reference.Owner.Name.Equals("ГОСТ 10704-91 Трубы стальные электросварные прямошовные")
+                   select reference.Owner;
+        }
+
         private static void Change(Document doc, IEnumerable<FamilyInstance> welds, string color, string pipeType)
         {
             ElementId material = new FilteredElementCollector(doc).OfClass(typeof(Material)).FirstOrDefault(m => m.Name.Equals(color))?.Id;
 
-            var chemicalPipe = from e in welds
-                               from Connector connector in e.MEPModel.ConnectorManager.Connectors
-                               from Connector reference in connector.AllRefs
-                               where reference.Owner.Name.Contains(pipeType)
-                               select e;
+            IEnumerable<FamilyInstance> pipeTypes = GetPipeType(welds, pipeType);
 
-            var fittingConnectors = from e in chemicalPipe
-                                    from Connector connector in e.MEPModel.ConnectorManager.Connectors
-                                    from Connector reference in connector.AllRefs
-                                    where (reference.Owner.Category.Id.IntegerValue == (int)BuiltInCategory.OST_PipeFitting)
-                                          && !reference.Owner.Name.Equals("ГОСТ 10704-91 Трубы стальные электросварные прямошовные")
-                                    select reference.Owner;
+            IEnumerable<Element> connectorsToRecolor = GetElementsToRecolor(pipeTypes);
 
-            foreach (Element element in fittingConnectors)
+            foreach (Element element in connectorsToRecolor)
             {
                 Parameter p = element.GetOrderedParameters().FirstOrDefault(e => e.Definition.Name.Equals("МатериалФитинга"));
                 p?.Set(material);
+            }
+        }
+
+        private static void ChangeColorOneQuery(Document doc)
+        {
+            var chemicalPipe = new FilteredElementCollector(doc).WhereElementIsNotElementType()
+                .WhereElementIsViewIndependent()
+                .OfCategory(BuiltInCategory.OST_PipeFitting)
+                .OfClass(typeof(FamilyInstance))
+                .Cast<FamilyInstance>()
+                .Where(i => i.Name.Equals("ГОСТ 10704-91 Трубы стальные электросварные прямошовные") && i.Symbol.FamilyName.Equals("801_СварнойШов_ОБЩИЙ"))
+                .SelectMany(e => e.MEPModel.ConnectorManager.Connectors.Cast<Connector>(), (e, connector) => (e, connector))
+                .SelectMany(t => t.connector.AllRefs.Cast<Connector>(), (familyInstance, reference) => (familyInstance, reference))
+                .Where(
+                    t => t.reference.Owner.Name.Contains("Азот") || t.reference.Owner.Name.Contains("Вода") || t.reference.Owner.Name.Contains("Газ")
+                         || t.reference.Owner.Name.Contains("Дренаж") || t.reference.Owner.Name.Contains("Канализация") || t.reference.Owner.Name.Contains("Нефтепродукты")
+                         || t.reference.Owner.Name.Contains("Пенообразователь") || t.reference.Owner.Name.Contains("ХимическиеРеагенты"))
+                .Select((t, f) => (t.familyInstance.e, t.reference.Owner))
+                .SelectMany(e => e.e.MEPModel.ConnectorManager.Connectors.Cast<Connector>(), (e, connector) => (familyInstance: e.e, connector, Owner: e.Owner))
+                .SelectMany(t => t.connector.AllRefs.Cast<Connector>(), (t, reference) => (t, reference))
+                .Where(
+                    t => (t.reference.Owner.Category.Id.IntegerValue == (int)BuiltInCategory.OST_PipeFitting)
+                         && !t.reference.Owner.Name.Equals("ГОСТ 10704-91 Трубы стальные электросварные прямошовные"))
+                .GroupBy(e => e.t.Owner.Name, e => e.reference.Owner)
+                .ToDictionary(e => e.Key, e => e.ToList());
+
+            using (Transaction tran = new Transaction(doc))
+            {
+                tran.Start("Change");
+
+                foreach (KeyValuePair<string, List<Element>> valuePair in chemicalPipe)
+                {
+                    if (valuePair.Key.Contains("Азот"))
+                    {
+                        ElementId material = new FilteredElementCollector(doc).OfClass(typeof(Material)).FirstOrDefault(m => m.Name.Equals("0_153_255"))?.Id;
+                        foreach (Element element in valuePair.Value)
+                        {
+                            Parameter p = element.GetOrderedParameters().FirstOrDefault(e => e.Definition.Name.Equals("МатериалФитинга"));
+                            p?.Set(material);
+                        }
+                    }
+                }
+
+                tran.Commit();
             }
         }
     }
