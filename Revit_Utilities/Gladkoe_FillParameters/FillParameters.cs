ececed9ef8a780f8e17b4f91520e92132af02f56
Shenn2948 <shenn2948@gmail.com>
2019-09-10 13:08:46 +0300

    added parameters filling

--- Revit_Utilities/Gladkoe_FillParameters/FillParameters.cs
+++ Revit_Utilities/Gladkoe_FillParameters/FillParameters.cs
@@ -1,5 +1,6 @@
 ﻿namespace Revit_Utilities.Gladkoe_FillParameters
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
@@ -10,24 +11,67 @@
     {
         public static void GetElements(Document doc, UIDocument uidoc)
         {
-            var elements = new FilteredElementCollector(doc).WhereElementIsNotElementType()
-                .WhereElementIsViewIndependent()
-                .WherePasses(new ElementMulticategoryFilter(new List<BuiltInCategory> { BuiltInCategory.OST_PipeAccessory, BuiltInCategory.OST_MechanicalEquipment }))
-                .OfClass(typeof(FamilyInstance))
-                .Cast<FamilyInstance>()
-                .Where(
-                    e => (e.SuperComponent == null) && (e.MEPModel.ConnectorManager != null) && !e.Symbol.FamilyName.Equals("802_ОпорыКорпусныеПриварные_КП_ОСТ36-146-88(ОбМод)"))
-                .SelectMany(e => e.MEPModel.ConnectorManager.Connectors.Cast<Connector>(), (instance, connector) => (instance, connector))
-                .SelectMany(e => e.connector.AllRefs.Cast<Connector>(), (tupleInstanceConnector, connector) => (tupleInstanceConnector, connector))
-                .SelectMany(e => e.connector.ConnectorManager.Connectors.Cast<Connector>(), (tupleInstanceConnector, connector) => (tupleInstanceConnector, connector))
-                .SelectMany(e => e.connector.AllRefs.Cast<Connector>(), (tupleInstanceConnector, connector) => (tupleInstanceConnector, connector))
-                .Where(
-                    e => (e.connector.Owner.Category.Id.IntegerValue == (int)BuiltInCategory.OST_PipeCurves)
-                         || (e.connector.Owner.Category.Id.IntegerValue == (int)BuiltInCategory.OST_PipeFitting))
-                .Select(e => e.connector.Owner.Id)
-                .ToList();
+            // концевое условие - фланец
+            List<ElementId> flanges = GetFlange(doc);
 
-            var elements2 = new FilteredElementCollector(doc).WhereElementIsNotElementType()
+            // концевое условие - сварка
+            List<ElementId> welds = GetWelds(doc, flanges);
+
+            SetParameters(doc, flanges, "Концевое условие", "Фланец", "Концевое условие 2", "Сварной шов");
+            SetParameters(doc, welds, "Концевое условие", "Сварной шов", "Концевое условие 2", "Сварной шов");
+        }
+
+        private static void SetParameters(
+            Document doc,
+            List<ElementId> elementsId,
+            string parameterName,
+            string parameterValue,
+            string parameterName2 = null,
+            string parameterValue2 = null)
+        {
+            IEnumerable<Element> elements = elementsId.Select(doc.GetElement);
+
+            foreach (Element element in elements)
+            {
+                Parameter p = element.GetOrderedParameters().FirstOrDefault(e => e.Definition.Name.Equals(parameterName)) ?? throw new ArgumentNullException(
+                                  nameof(p),
+                                  "Проблема в нахождении параметра \"МатериалФитинга\", проверьте наименования параметров");
+                p.Set(parameterValue);
+                if (parameterName2 != null)
+                {
+                    p = element.GetOrderedParameters().FirstOrDefault(e => e.Definition.Name.Equals(parameterName2)) ?? throw new ArgumentNullException(
+                            nameof(p),
+                            "Проблема в нахождении параметра \"МатериалФитинга\", проверьте наименования параметров");
+                    p.Set(parameterValue2);
+                }
+            }
+        }
+
+        private static List<ElementId> GetWelds(Document doc, List<ElementId> flange)
+        {
+            return new FilteredElementCollector(doc).WhereElementIsNotElementType()
+                .WhereElementIsViewIndependent()
+                .WherePasses(new ElementMulticategoryFilter(new List<BuiltInCategory> { BuiltInCategory.OST_PipeFitting, BuiltInCategory.OST_PipeCurves }))
+                .ToElements()
+                .Where(
+                    e =>
+                    {
+                        if (doc.GetElement(e.Id) is FamilyInstance s)
+                        {
+                            return !s.Symbol.FamilyName.Equals("801_СварнойШов_ОБЩИЙ") && (s.SuperComponent == null) && (s.MEPModel.ConnectorManager != null)
+                                   && !s.Symbol.FamilyName.Equals("802_ОпорыКорпусныеПриварные_КП_ОСТ36-146-88(ОбМод)");
+                        }
+
+                        return true;
+                    })
+                .Select(e => e.Id)
+                .Except(flange)
+                .ToList();
+        }
+
+        private static IEnumerable<ElementId> GetFlangeWithoutWelding(Document doc)
+        {
+            return new FilteredElementCollector(doc).WhereElementIsNotElementType()
                 .WhereElementIsViewIndependent()
                 .WherePasses(new ElementMulticategoryFilter(new List<BuiltInCategory> { BuiltInCategory.OST_PipeAccessory, BuiltInCategory.OST_MechanicalEquipment }))
                 .OfClass(typeof(FamilyInstance))
@@ -51,9 +95,42 @@
                 .Where(
                     e => (e.connector.Owner.Category.Id.IntegerValue == (int)BuiltInCategory.OST_PipeCurves)
                          || (e.connector.Owner.Category.Id.IntegerValue == (int)BuiltInCategory.OST_PipeFitting))
+                .Select(e => e.connector.Owner.Id);
+        }
+
+        private static List<ElementId> GetFlange(Document doc)
+        {
+            IEnumerable<ElementId> flangeWithoutWelding = GetFlangeWithoutWelding(doc);
+
+            return new FilteredElementCollector(doc).WhereElementIsNotElementType()
+                .WhereElementIsViewIndependent()
+                .WherePasses(new ElementMulticategoryFilter(new List<BuiltInCategory> { BuiltInCategory.OST_PipeAccessory, BuiltInCategory.OST_MechanicalEquipment }))
+                .OfClass(typeof(FamilyInstance))
+                .Cast<FamilyInstance>()
+                .Where(
+                    e => (e.SuperComponent == null) && (e.MEPModel.ConnectorManager != null) && !e.Symbol.FamilyName.Equals("802_ОпорыКорпусныеПриварные_КП_ОСТ36-146-88(ОбМод)"))
+                .SelectMany(e => e.GetSubComponentIds(), (instance, id) => (instance, subComponent: doc.GetElement(id) as FamilyInstance))
+                .Where(f => f.subComponent.Symbol.FamilyName.Contains("_Фланец_"))
+                .SelectMany(e => e.instance.MEPModel.ConnectorManager.Connectors.Cast<Connector>(), (instance, connector) => (instance, connector))
+                .SelectMany(e => e.connector.AllRefs.Cast<Connector>(), (tupleInstanceConnector, connector) => (tupleInstanceConnector, connector))
+                .Where(
+                    e =>
+                    {
+                        if (doc.GetElement(e.connector.Owner.Id) is FamilyInstance s)
+                        {
+                            return s.Symbol.FamilyName.Equals("801_СварнойШов_ОБЩИЙ");
+                        }
+
+                        return false;
+                    })
+                .SelectMany(e => e.connector.ConnectorManager.Connectors.Cast<Connector>(), (tupleInstanceConnector, connector) => (tupleInstanceConnector, connector))
+                .SelectMany(e => e.connector.AllRefs.Cast<Connector>(), (tupleInstanceConnector, connector) => (tupleInstanceConnector, connector))
+                .Where(
+                    e => (e.connector.Owner.Category.Id.IntegerValue == (int)BuiltInCategory.OST_PipeCurves)
+                         || (e.connector.Owner.Category.Id.IntegerValue == (int)BuiltInCategory.OST_PipeFitting))
                 .Select(e => e.connector.Owner.Id)
+                .Union(flangeWithoutWelding)
                 .ToList();
-            uidoc.Selection.SetElementIds(elements2);
         }
     }
 }
