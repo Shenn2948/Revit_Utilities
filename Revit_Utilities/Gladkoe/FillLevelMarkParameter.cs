d9136617a0685ad047a557b9555053db3e89540f
Andrew Kozhevnikov <andrew.kozhevnikov93@gmail.com>
2019-09-13 15:20:00 +0300

    implemented getting start and end pipe's offset

--- Revit_Utilities/Gladkoe/FillLevelMarkParameter.cs
+++ Revit_Utilities/Gladkoe/FillLevelMarkParameter.cs
@@ -7,9 +7,17 @@ using Autodesk.Revit.UI;
 
 namespace Revit_Utilities.Gladkoe
 {
+    using System.Diagnostics;
     using System.Text;
 
+    using Autodesk.Revit.Creation;
+    using Autodesk.Revit.DB.Mechanical;
+    using Autodesk.Revit.DB.Plumbing;
     using Autodesk.Revit.UI.Selection;
+
+    using Revit_Utilities.Utilities;
+
+    using Document = Autodesk.Revit.DB.Document;
 
     public class FillLevelMarkParameter
     {
@@ -18,6 +26,8 @@ namespace Revit_Utilities.Gladkoe
             try
             {
                 FillParametersAction(doc, uidoc);
+
+                //Elevation2(doc);
             }
             catch (Exception e)
             {
@@ -28,7 +38,6 @@ namespace Revit_Utilities.Gladkoe
         private static void FillParametersAction(Document doc, UIDocument uidoc)
         {
             Reference pickedObj = uidoc.Selection.PickObject(ObjectType.Element, "Select element");
-            ElementId elementId = pickedObj.ElementId;
 
             using (Transaction tx = new Transaction(doc))
             {
@@ -37,11 +46,56 @@ namespace Revit_Utilities.Gladkoe
 
                 Element e = doc.GetElement(pickedObj.ElementId);
 
-                sb.Append("\n" + e.Name);
+                LocationCurve lc = e.Location as LocationCurve;
+                Curve c = lc.Curve;
+
+                sb.Append("\n" + $"Pipe {e.Id.IntegerValue} from {c.GetEndPoint(0).Z.FeetAsMillimeters()} to { c.GetEndPoint(1).Z.FeetAsMillimeters()}");
+
                 TaskDialog.Show("Info", sb.ToString());
 
-                tx.Commit()
+                tx.Commit();
             }
+        }
+
+        private static void Elevation(Document doc, Element element)
+        {
+            BasePoint projectPoint = new FilteredElementCollector(doc).OfClass(typeof(BasePoint)).Cast<BasePoint>().First(x => !x.IsShared);
+
+            var px = projectPoint.get_Parameter(BuiltInParameter.BASEPOINT_EASTWEST_PARAM).AsDouble();
+            var py = projectPoint.get_Parameter(BuiltInParameter.BASEPOINT_NORTHSOUTH_PARAM).AsDouble();
+            var pz = projectPoint.get_Parameter(BuiltInParameter.BASEPOINT_ELEVATION_PARAM).AsDouble();
+            XYZ project = new XYZ(px, py, pz);
+
+            if (element.Location is LocationCurve lc)
+            {
+            }
+
+            // var elementPoint = loc.Point.Subtract(project).Z;
+            // return elementPoint;
+        }
+
+        private static void GetStartToEndOffset(Document doc)
+        {
+            var a = new FilteredElementCollector(doc).OfClass(typeof(Pipe)).Cast<Pipe>();
+
+            int numDucts = 0;
+            int numCurves = 0;
+            StringBuilder sb = new StringBuilder();
+            foreach (Pipe d in a)
+            {
+                ++numDucts;
+
+                LocationCurve lc = d.Location as LocationCurve;
+
+                ++numCurves;
+
+                Curve c = lc.Curve;
+                sb.Append("\n" + $"Pipe {d.Id.IntegerValue} from {c.GetEndPoint(0).Z.FeetAsMillimeters()} to { c.GetEndPoint(1).Z.FeetAsMillimeters()}");
+            }
+
+            sb.AppendLine();
+            sb.Append($"{numDucts} Pipes analysed, and {numCurves} curve listed.");
+            TaskDialog.Show("I", sb.ToString());
         }
     }
 }
---
+++ Revit_Utilities/Utilities/ParameterUnitConverter.cs
@@ -0,0 +1,33 @@
+﻿namespace Revit_Utilities.Utilities
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Autodesk.Revit.DB;
+
+    public static class ParameterUnitConverter
+    {
+        private const double MetersInFeet = 0.3048;
+
+        public static double FeetAsMeters(this double param)
+        {
+            double imperialValue = param;
+
+            return imperialValue * MetersInFeet; // feet
+        }
+
+        public static double FeetAsMillimeters(this double param)
+        {
+            double imperialValue = param;
+
+            return imperialValue * MetersInFeet * 1000;
+        }
+
+        public static double FeetAsCentimeters(this double param)
+        {
+            double imperialValue = param;
+
+            return imperialValue * MetersInFeet * 100;
+        }
+    }
+}
